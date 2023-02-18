807b04419d6d9bbc90925bf04d362cbbc6391990
“Jimmy <kyc46@sfu.ca>
2023-02-18 10:11:31 -0800

    jimmy make some changes

--- python_is_cool.py
+++ python_is_cool.py
@@ -1,7 +1,7 @@
 # look! I make comments with hashtags!
 import pygame
 import turtle
-my_array = ["There are strings and ints in this array!", 2, 3, 4, 5]
+my_array = ["There are strings and ints in this array!", 2, 3, 4, 5, 7, 8]
 
 for i in range(len(my_array)):
     print(my_array[i])

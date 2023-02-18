ed20d9dc3fab172883fb20086805f652ac69ea63
kyc46 <kyc46@sfu.ca>
2023-02-17 23:05:36 -0800

    test file for java

---
+++ main.java
@@ -0,0 +1,20 @@
+class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int sub(int a, int b) {
+        return a - b;
+    }
+
+
+}

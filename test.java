d86c34a8db2d679e3cd14039d24f38a8ffa56d40
kyc46 <kyc46@sfu.ca>
2023-02-18 10:15:41 -0800

    change to one helloworld

--- test.java
+++ test.java
@@ -1,20 +1,6 @@
-class test {
+class tests {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
+        System.out.println("Hello World!");
     }
-
-    int add(int a, int b) {
-        return a + b;
-    }
-
-    int sub(int a, int b) {
-        return a - b;
-    }
-
-
-}
+    
+} 

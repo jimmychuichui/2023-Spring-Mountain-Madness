7d37ac237994329d57e2033666c9f74c41a64277
kyc46 <kyc46@sfu.ca>
2023-02-18 12:12:19 -0800

    file system design for the game

--- start.py
+++ start.py
@@ -1,12 +1,40 @@
 #this is for starting the game
 
 import chapter_1
+import os.path
+import csv
+import helper_functions
 
 def main():
     print("Hello traveller, weclome to Paris!")
+
+
+    #read data.csv file
+    #if it is not exisiting, create a new one, let the user to input the data, start the game from the beginning
+    #if it is exisiting, read the data, start the game from the last checkpoint
+    if os.path.exists("data.csv"):
+        #read the data decide which chapter to start
+        helper_functions.chapeter_selecter()
+        return
+
+
+
+
     print("What is your name?")
 
     name = input()
+    chapter = 1
+    #create and write the name and the chapter to the data.csv file
+    with open("data.csv", "w") as file:
+
+        # create a CSV writer object
+        writer = csv.writer(file)
+        # write the name to the CSV file
+        writer.writerow([name])
+        writer.writerow([chapter])
+
+    
+   
 
     print("Hello " + name + ", it is a pleasure to meet you!")
     print("Your mission is to find the treasure in the city of Paris.")
@@ -21,7 +49,7 @@ def main():
         option1 = input()
         if option1 == "1":
             flag = False
-            chapter_1.branch1()
+            chapter_1.branch1(name)
             
         elif option1 == "2":
             flag = False

9a2faf64fa3d75a793e05c399bb5100a90c84bab
Jonathan Peters <jmp22@sfu.ca>
2023-02-18 11:09:55 -0800

    removed unessessary files

--- python_is_cool.py
+++
@@ -1,8 +0,0 @@
-# look! I make comments with hashtags!
-import pygame
-import turtle
-my_array = ["There are strings and ints in this array!", 2, 3, 4, 5]
-
-for i in range(len(my_array)):
-    print(my_array[i])
-

515f0db06fd8e06a145e3656344e809856d03fdb
kyc46 <kyc46@sfu.ca>
2023-02-18 23:06:24 -0800

    read text

--- read_text.py
+++ read_text.py
@@ -4,14 +4,21 @@ def read_for_diaglog(node_number, node_name):
     with open("Dialogue.txt", "r") as file:
         lines = file.readlines()
 
+    start_index = 0
+    end_index = 0
     for i, line in enumerate(lines):
         if line.startswith("**") and node_name in line and str(node_number) in line:
             i+=1
+            start_index = i
             while i < len(lines) and not lines[i].startswith("**"):
                 print(lines[i], end="")
                 i += 1
+            end_index = i
+            
+    
+    return start_index+1, end_index
 
-read_for_diaglog(5, "Z")
 
-
+index = read_for_diaglog(6, "Z")
+print(index)
     

fa4c3ded41afe952f05bc0f99eaed6dc10bd1f37
Xander <xws@sfu.ca>
2023-02-18 18:58:38 -0800

    print_narrate: prints text from Dialogue.txt print_Cyan: prints in cyan print_garabage: prints french #size of random french words

--- Term_Disp.py
+++ Term_Disp.py
@@ -1,6 +1,8 @@
 import time
-import colorama
+from colorama import Fore, Back, Style
 import random
+
+
 
 i = 0
 j = 0
@@ -8,7 +10,7 @@ j = 0
 #end_int is the digit where the line ends in the file
 #delay_scalar is the multiple with which the size of the target sentence is multiplied for a general effect
 #attention_const measures delay from one line printing to the next
-def Narrate(start_int, end_int, delay_scalar, attention_const):
+def print_Narrate(start_int, end_int, delay_scalar, attention_const):
     start_index = start_int-1
     text = open('Dialogue.txt')
     file = text.readlines()
@@ -20,4 +22,28 @@ def Narrate(start_int, end_int, delay_scalar, attention_const):
         time.sleep(attention_const+(delay_scalar*time_of_sentence))
     print(62*'/') 
 
+def print_Cyan(txt):
+    print(f"{Style.BRIGHT}{Fore.CYAN}"+txt.strip('\n'),f"{Style.RESET_ALL}", end = "")
 
+def print_Outcome(attempts, start_int):
+    lst = [100, 87.5, 75, 62.5, 50, 37.5, 25, 12.5]
+    percent = lst[attempts-1]
+    checker = random.randint(1,100)
+    file = open("Translations.txt")
+    line = file.readlines()
+    if percent>=checker:
+        success = 0
+    else:
+        success = 1
+    print("Chance of success {:.2f}".format(percent))
+    print(success)
+    print(line[start_int+success])
+
+def print_garbage(size):
+    file = open("French Words.txt")
+    line = file.readlines()
+    for i in range(size):
+        random_num = random.randint(0,99)
+        print_Cyan((line[random_num]))
+
+print_garbage(17)

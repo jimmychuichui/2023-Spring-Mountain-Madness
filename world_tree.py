9bb4d23b8bc9eac54a03351121900dc4e508cdbd
Jonathan Peters <jmp22@sfu.ca>
2023-02-18 18:23:03 -0800

    Updated node creation function

--- world_tree.py
+++ world_tree.py
@@ -11,73 +11,88 @@ class Node:
         return self.data
 
 
-node_start = Node("Start")
+
+def create_nudes():
+    global node_start
+    global node_1
+    global node_2
+    global node_3
+    global node_4
+    global node_5
+    global node_6
+    global node_7
+    global node_8
+    global node_9
+    global node_10
+    global node_11
+    global node_12
+    global node_end
+    node_start = Node("Start")
+
+    # create room nodes
+    node_1 = Node("1")
+
+    node_2 = Node("2")
+
+    node_3 = Node("3")
+
+    node_4 = Node("4")
+
+    node_5 = Node("5")
+
+    node_6 = Node("6")
+
+    node_7 = Node("7")
+
+    node_8 = Node("8")
+
+    node_9 = Node("9")
+
+    node_10 = Node("10")
+
+    node_11 = Node("11")
+
+    node_12 = Node("12")
+
+    node_end = Node("End")
 
 
+    # link the rooms together
 
-# create room nodes
-node_1 = Node("1")
+    node_start.optionL = node_1
+    node_start.optionR = node_2
 
-node_2 = Node("2")
+    node_1.optionL = node_3
+    node_1.optionR = node_4
 
-node_3 = Node("3")
+    node_2.optionL = node_5
+    node_2.optionR = node_6
 
-node_4 = Node("4")
+    node_3.optionR = node_7
 
-node_5 = Node("5")
+    node_4.optionL = node_7
+    node_4.optionR = node_8
 
-node_6 = Node("6")
+    node_5.optionL = node_8
+    node_5.optionR = node_9
 
-node_7 = Node("7")
+    node_6.optionL = node_9
 
-node_8 = Node("8")
+    node_7.optionR = node_10
 
-node_9 = Node("9")
+    node_8.optionL = node_10
+    node_8.optionR = node_11
 
-node_10 = Node("10")
+    node_9.optionL = node_11
 
-node_11 = Node("11")
+    node_10.optionR = node_12
 
-node_12 = Node("12")
+    node_11.optionL = node_12
 
-node_end = Node("End")
-
-
-# link the rooms together
-
-node_start.optionL = node_1
-node_start.optionR = node_2
-
-node_1.optionL = node_3
-node_1.optionR = node_4
-
-node_2.optionL = node_5
-node_2.optionR = node_6
-
-node_3.optionR = node_7
-
-node_4.optionL = node_7
-node_4.optionR = node_8
-
-node_5.optionL = node_8
-node_5.optionR = node_9
-
-node_6.optionL = node_9
-
-node_7.optionR = node_10
-
-node_8.optionL = node_10
-node_8.optionR = node_11
-
-node_9.optionL = node_11
-
-node_10.optionR = node_12
-
-node_11.optionL = node_12
-
-node_12.optionL = node_end
+    node_12.optionL = node_end
 
 
+create_nudes()
 
 node_itr = node_start
 dead = False
